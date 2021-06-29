20a580d0b9793d12a1e2647430e1fefcf0d3d07d
viest1 <viest1994@gmail.com>
2021-06-29 18:07:27 +0200

    version 1.1 progress bar animated

--- js/observers.js
+++ js/observers.js
@@ -4,7 +4,7 @@ const sectionOne = document.querySelector("main")
 
 const sectionOneOptions = {
     threshold:0,
-    rootMargin:"-700px 0px 0px 0px"
+    rootMargin:"-750px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
@@ -32,6 +32,14 @@ const sliders1 = document.querySelectorAll(".animation-slide-right")
 const sliders2 = document.querySelectorAll(".animation-slide-top")
 const sliders3 = document.querySelectorAll(".animation-slide-bottom")
 const sliders4 = document.querySelectorAll(".animation-slide-testimonial")
+const counter1 = document.querySelectorAll(".counter-first")
+const counter2 = document.querySelectorAll(".counter-second")
+const counter3 = document.querySelectorAll(".counter-third")
+const counter4 = document.querySelectorAll(".counter-fourth")
+const progress1 = document.querySelectorAll(".progress-bar-1")
+const progress2 = document.querySelectorAll(".progress-bar-2")
+const progress3 = document.querySelectorAll(".progress-bar-3")
+const progress4 = document.querySelectorAll(".progress-bar-4")
 
 const appearOptions = {
     threshold: 0.75,
@@ -111,7 +119,109 @@ const appearOnScrollTestimonial = new IntersectionObserver(function(entries, app
 },
 appearOptionsTestimonial)
 
+// Counter-1
+const counter11 = new IntersectionObserver(function(entries, counter11){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("counter-1");
+            counter11.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
 
+// Counter-2
+const counter22 = new IntersectionObserver(function(entries, counter22){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("counter-2");
+            counter22.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
+
+// Counter-3
+const counter33 = new IntersectionObserver(function(entries, counter33){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("counter-3");
+            counter33.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
+
+// Counter-4
+const counter44 = new IntersectionObserver(function(entries, counter44){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("counter-4");
+            counter44.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
+
+// Progress bar 1
+const progressbar1 = new IntersectionObserver(function(entries, progressbar1){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("progress-bar-11");
+            progressbar1.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
+
+// Progress bar 2
+const progressbar2 = new IntersectionObserver(function(entries, progressbar2){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("progress-bar-22");
+            progressbar2.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
+
+// Progress bar 3
+const progressbar3 = new IntersectionObserver(function(entries, progressbar3){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("progress-bar-33");
+            progressbar3.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
+
+// Progress bar 4
+const progressbar4 = new IntersectionObserver(function(entries, progressbar4){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add("progress-bar-44");
+            progressbar4.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
 
 // Slide to left
 sliders.forEach(slider => {
@@ -136,4 +246,44 @@ sliders3.forEach(slider => {
 // Slide to left - Testimonial
 sliders4.forEach(slider => {
     appearOnScrollTestimonial.observe(slider);
+})
+
+// Counter-1
+counter1.forEach(slider => {
+    counter11.observe(slider);
+})
+
+// Counter-2
+counter2.forEach(slider => {
+    counter22.observe(slider);
+})
+
+// Counter-3
+counter3.forEach(slider => {
+    counter33.observe(slider);
+})
+
+// Counter-4
+counter4.forEach(slider => {
+    counter44.observe(slider);
+})
+
+// Progress bar 1
+progress1.forEach(slider => {
+    progressbar1.observe(slider);
+})
+
+// Progress bar 2
+progress2.forEach(slider => {
+    progressbar2.observe(slider);
+})
+
+// Progress bar 3
+progress3.forEach(slider => {
+    progressbar3.observe(slider);
+})
+
+// Progress bar 4
+progress4.forEach(slider => {
+    progressbar4.observe(slider);
 })
